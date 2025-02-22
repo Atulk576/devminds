1483338274331c0b6f952b21aac25c16e2c342fc
Atul Kumar <coderunner576@gmail.com>
2025-02-22 10:51:14 +0530

    Delete js/faq-header.js

--- js/faq-header.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".faq-header").forEach((header) => {
-    header.addEventListener("click", () => {
-        const parent = header.parentNode;
-        const icon = header.querySelector(".faq-icon");
-
-        parent.classList.toggle("active");
-        icon.textContent = parent.classList.contains("active") ? "−" : "+";
-    });
-});
